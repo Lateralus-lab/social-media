b562ce9c83fe5f0bf8408268d7df17a5fa8d4557
Lateralus <zoofoo.online@gmail.com>
2021-01-03 23:42:45 +0000

    added redirect to login when user is not logged in

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import { Redirect } from 'react-router-dom';
 // Import components
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
@@ -25,6 +26,8 @@ const Dialogs = (props) => {
     let body = e.target.value;
     props.updateNewMessageBody(body);
   };
+
+  if (!props.isAuth) return <Redirect to={'/login'} />;
 
   return (
     <div className={s.dialogs}>
