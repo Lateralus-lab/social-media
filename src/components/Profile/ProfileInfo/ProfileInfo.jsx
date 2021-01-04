a382272886c7b306caea43cf24386222611d4e42
Lateralus <zoofoo.online@gmail.com>
2021-01-04 18:25:44 +0000

    added profile status feature

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -13,6 +14,7 @@ const ProfileInfo = (props) => {
       <div className={s.descBlock}>
         <img src={props.profile.photos.large} alt="photo" />
         <div>Ava + desc</div>
+        <ProfileStatus status={'Yo!'} />
       </div>
     </div>
   );
